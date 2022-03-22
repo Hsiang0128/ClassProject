ad702e941a81a2daf877a451307d7c5a0276e9e2
Hsiang0128 <100186660+Hsiang0128@users.noreply.github.com>
2022-03-22 14:57:59 +0800

    is ok?

--- polynomial_evaluation.c
+++ polynomial_evaluation.c
@@ -1,3 +1,5 @@
+/*
+//===============================old=============================//
 #include <stdio.h>
 #include <math.h>
 double val=0,result=0,a,b;
@@ -13,6 +15,33 @@ int main() {
             numOfItem = b;
         }
         else result += (pow(val,b))*a;
+    }
+    printf("%lf\n", result);
+}
+*/
+//===============================new=============================//
+#include <stdio.h>
+double power(double x, int q) {
+    if (q == 0) {
+        return 1;
+    }
+    double m = x;
+    for (int i = 1; i < q; i++) {
+        m*=x;
+    }
+    return m;
+}
+int main() {
+    printf("請輸入浮點數x的值以及多項式含有的項數正整數y︰");
+    double x;
+    int y;
+    scanf("%lf%d", &x, &y);
+    double result = 0.0;
+    double p[6];
+    int q[6];
+    for (int i = 0; i < y; i++) {
+        scanf("%lf%d", &p[i], &q[i]);
+        result += p[i] * power(x, q[i]);
     }
     printf("%lf\n", result);
 }
