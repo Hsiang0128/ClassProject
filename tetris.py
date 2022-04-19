f6bb4fa6fb020d2abc58a3b868ecaa5c22bee3d5
Hsiang0128 <100186660+Hsiang0128@users.noreply.github.com>
2022-04-19 15:44:25 +0800

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -92,7 +92,7 @@ class Tetris(object):
                     self.active_block.move(-constants.BWIDTH,0)
                 if ev.key == pygame.K_RIGHT:
                     self.active_block.move(constants.BWIDTH,0)
-                if ev.key == pygame.K_SPACE:
+                if ev.key == pygame.K_w:
                     self.active_block.rotate()
                 if ev.key == pygame.K_p:
                     self.pause()
